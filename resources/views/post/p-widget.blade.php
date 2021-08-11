d87d5e2679475d5dffd1facae698f01e5d4ea2d9
IRIS Vu <longgxhc123@gmail.com>
2021-08-11 22:09:56 +0700

    widget sidebar

---
+++ resources/views/post/p-widget.blade.php
@@ -0,0 +1,10 @@
+<div class="media asider__category-pos">
+    <div class="media-body">
+      <h3 class="asider__aside">
+        <a href="{{ home_url('/') }}{!! $postw->post_name  !!}" title="" rel="bookmark">
+            {!! $postw->post_title  !!}
+        </a>
+      </h3>
+    </div>
+    <img class="ml-1 asider__img" src="{!!wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' )!!}" alt="">
+</div>
